f2a18f65621c8f028e7b5108bbf66693e74ed244
codyolsendummy <codyolsendummy@gmail.com>
2022-08-03 09:29:15 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ components/avatar.js
@@ -0,0 +1,19 @@
+import Image from 'next/future/image'
+import { urlForImage } from '../lib/sanity'
+
+export default function Avatar({ name, picture }) {
+  return (
+    <div className="flex items-center">
+      <div className="relative w-12 h-12 mr-4">
+        <Image
+          src={urlForImage(picture).height(96).width(96).fit('crop').url()}
+          className="rounded-full"
+          height={96}
+          width={96}
+          alt={name}
+        />
+      </div>
+      <div className="text-xl font-bold">{name}</div>
+    </div>
+  )
+}
